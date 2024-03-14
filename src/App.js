429e02d14dad8fa894f8954555cd99c952f7804c
Nikolaj <niko.sage@gmail.com>
2024-03-14 14:51:15 -0700

    updated download page for most recent app version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { GallerySplash } from './Components/GallerySplash/GallerySplash';
+import { Title } from './Components/Title/Title';
+import { Info } from './Components/Info/Info';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <GallerySplash />
+      <Info />
+      
     </div>
   );
 }
